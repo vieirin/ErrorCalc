dbbf5bede5ee98d281bc95b80902b3c4ea9ae293
Vieira Neto <vieiranetoc@gmail.com>
2017-09-21 06:17:58 -0300

    comma and backspace working

--- display.py
+++ display.py
@@ -11,11 +11,25 @@ class Display(Gtk.Entry):
         self.set_editable(False)
 
     def on_key_release(self, widget, event):
-        acceptable_entry = [x for x in range(48, 58)] + [x for x in range(65456, 65466)]
-        if event.keyval in acceptable_entry:
+        acceptable_numbers = [x for x in range(48, 58)] + [x for x in range(65456, 65466)]
+        commas_keys = [44, 46, 65454]
+        if event.keyval in acceptable_numbers:
             if event.keyval in range(65456, 65466):
                 event.keyval -= 65408
             curr_txt = widget.get_text()
-            curr_txt += chr(event.keyval)
+            curr_txt += chr(event.keyval) 
             widget.set_text(curr_txt)
-        
+        elif event.keyval in commas_keys:
+            #convert ',' to '.' and allow only one comma
+            old_txt = widget.get_text()
+            new_txt = old_txt + '.'
+            if not old_txt:
+                new_txt = ''
+            if new_txt.count('.') > 1: 
+                new_txt = new_txt[:-1]
+            widget.set_text(new_txt)
+        elif event.keyval == 65288:
+            #backspace case
+            curr_txt = widget.get_text()
+            curr_txt = curr_txt[:-1]
+            widget.set_text(curr_txt)
--- numbers.py
+++ numbers.py
@@ -25,6 +25,8 @@ class NumbersGrid(Gtk.Grid):
         plus_minus_button = Gtk.Button(label='±')
         
         self.numbers.append(zero_button)
+        self.numbers.append(comma_button)
+        self.numbers.append(plus_minus_button)
 
         self.attach(zero_button, 0, 3, 1, 1)
         self.attach(comma_button, 1, 3, 1, 1)
@@ -34,9 +36,9 @@ class NumbersGrid(Gtk.Grid):
         #connect all grid buttons to main display
         self.display = display
         for button in self.numbers:
-            button.connect('clicked', self.on_button_clicked, self.display)
+            button.connect('clicked', self.on_number_clicked, self.display)
 
-    def on_button_clicked(self, widget, display):
+    def on_number_clicked(self, widget, display):
         curr_text = display.get_text()
         curr_text+=widget.get_label()
         display.set_text(curr_text)
